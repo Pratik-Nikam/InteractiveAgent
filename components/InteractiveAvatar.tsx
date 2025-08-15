100ac40238f8505e087656de0a985a6ee1c8ea66
demonbyte <prajaktap094@gmail.com>
2025-08-15 12:32:47 -0500

    updated default knowledge base id

--- components/InteractiveAvatar.tsx
+++ components/InteractiveAvatar.tsx
@@ -25,7 +25,7 @@ import { AVATARS } from "@/app/lib/constants";
 const DEFAULT_CONFIG: StartAvatarRequest = {
   quality: AvatarQuality.Low,
   avatarName: AVATARS[0].avatar_id,
-  knowledgeId: undefined,
+  knowledgeId: "07df2c9d689742e3a56735ab6e491984",
   voice: {
     rate: 1.5,
     emotion: VoiceEmotion.EXCITED,
